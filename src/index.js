9690d4d7f9896973dd4829b4d1faac0fd6e7d5ee
briant <briantneira@hotmail.com>
2023-07-06 21:30:42 -0500

    Proyecto Final rama desarrollo

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider domain='briantneira.us.auth0.com'
+      clientId='GWtoqsigVHkihFkevIcfwo8dfADvg2CW'
+      redirectUri={window.location.origin}>
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
